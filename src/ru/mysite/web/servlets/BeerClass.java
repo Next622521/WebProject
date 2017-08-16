c7cfc6ea6411f44057506a9a8f6ba2d34bb4a29e
igor <xnextx622521@gmail.com>
2017-08-16 13:38:06 +0300

    greate new model

--- src/ru/mysite/web/servlets/BeerClass.java
+++ src/ru/mysite/web/servlets/BeerClass.java
@@ -1,15 +1,21 @@
 package ru.mysite.web.servlets;
+import ua.mysite.web.model.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
 public class BeerClass extends HttpServlet {
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response)
             throws IOException, ServletException {
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List<String> result = be.getBrands(c);
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("Beer Selection Advice<br>");
-        String c = request.getParameter("color");
-        out.println("<br>Got beer coooooolor0 " + c);
-    }
+        for (String h:result)
+            out.print("<br>try: " +h );
+        }
+
 }
