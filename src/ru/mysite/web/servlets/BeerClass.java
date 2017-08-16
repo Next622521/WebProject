db7d6ad7c42f32f7a6f8744c8eca96821e7215f6
Igor <xnextx622521@gmail.com>
2017-08-16 09:22:24 +0300

    Initial commit

---
+++ src/ru/mysite/web/servlets/BeerClass.java
@@ -0,0 +1,15 @@
+package ru.mysite.web.servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+public class BeerClass extends HttpServlet {
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Beer Selection Advice<br>");
+        String c = request.getParameter("color");
+        out.println("<br>Got beer coolor " + c);
+    }
+}
